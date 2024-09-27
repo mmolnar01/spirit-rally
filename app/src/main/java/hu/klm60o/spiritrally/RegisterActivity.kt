35c0ff71313e032970c4318791b255b9562772e9
Molnár Márton <molnar.marton.hun@gmail.com>
2024-09-27 19:31:10 +0200

    RegisterActivity UI kész

--- app/src/main/java/hu/klm60o/spiritrally/RegisterActivity.kt
+++ app/src/main/java/hu/klm60o/spiritrally/RegisterActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,6 +28,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -112,7 +118,68 @@ fun RegisterScreen() {
                     .padding(0.dp, 10.dp, 0.dp, 0.dp)
             )
 
-            
+            //Jelszó bemeneti mező
+            OutlinedTextField(value = userPassword.value, onValueChange = {
+                userPassword.value = it
+            },
+                leadingIcon = {
+                    Icon(Icons.Default.Lock, contentDescription = "password")
+                },
+                label = {
+                    Text(text = "Jelszó")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 10.dp, 0.dp, 0.dp)
+            )
+
+            //Jelszó újra bemeneti mező
+            OutlinedTextField(value = userPasswordRepeat.value, onValueChange = {
+                userPasswordRepeat.value = it
+            },
+                leadingIcon = {
+                    Icon(Icons.Default.Lock, contentDescription = "passwordrepeat")
+                },
+                label = {
+                    Text(text = "Jelszó újra")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 10.dp, 0.dp, 0.dp)
+            )
+
+            //Regisztrálás gomb
+            OutlinedButton(onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 20.dp, 0.dp, 0.dp)) {
+                Text(text = "Regisztráció",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(5.dp),
+                    textAlign = TextAlign.Center,
+                    fontSize = 20.sp
+                )
+            }
+
+            //A bejelentkezéshez
+            Row(verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)) {
+                Text(text = "Már regisztráltál?",
+                    modifier = Modifier
+                        .padding(5.dp))
+                Text(text = "Bejelentkezés",
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .clickable { /*TODO*/ },
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+
         }
     }
 
