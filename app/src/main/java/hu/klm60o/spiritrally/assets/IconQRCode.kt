2f14cce1c603555f1ddbeb22c1df2f2e2b9b0ee5
Molnár Márton <molnar.marton.hun@gmail.com>
2024-10-12 11:28:04 +0200

    BottomAppBar Created

---
+++ app/src/main/java/hu/klm60o/spiritrally/assets/IconQRCode.kt
@@ -0,0 +1,238 @@
+package hu.klm60o.spiritrally.assets
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+public val QrCode: ImageVector
+    get() {
+        if (_QrCode != null) {
+            return _QrCode!!
+        }
+        _QrCode = ImageVector.Builder(
+            name = "QrCode",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(3.75f, 4.875f)
+                curveTo(3.75f, 4.2537f, 4.2537f, 3.75f, 4.875f, 3.75f)
+                horizontalLineTo(9.375f)
+                curveTo(9.9963f, 3.75f, 10.5f, 4.2537f, 10.5f, 4.875f)
+                verticalLineTo(9.375f)
+                curveTo(10.5f, 9.9963f, 9.9963f, 10.5f, 9.375f, 10.5f)
+                horizontalLineTo(4.875f)
+                curveTo(4.2537f, 10.5f, 3.75f, 9.9963f, 3.75f, 9.375f)
+                verticalLineTo(4.875f)
+                close()
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(3.75f, 14.625f)
+                curveTo(3.75f, 14.0037f, 4.2537f, 13.5f, 4.875f, 13.5f)
+                horizontalLineTo(9.375f)
+                curveTo(9.9963f, 13.5f, 10.5f, 14.0037f, 10.5f, 14.625f)
+                verticalLineTo(19.125f)
+                curveTo(10.5f, 19.7463f, 9.9963f, 20.25f, 9.375f, 20.25f)
+                horizontalLineTo(4.875f)
+                curveTo(4.2537f, 20.25f, 3.75f, 19.7463f, 3.75f, 19.125f)
+                verticalLineTo(14.625f)
+                close()
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(13.5f, 4.875f)
+                curveTo(13.5f, 4.2537f, 14.0037f, 3.75f, 14.625f, 3.75f)
+                horizontalLineTo(19.125f)
+                curveTo(19.7463f, 3.75f, 20.25f, 4.2537f, 20.25f, 4.875f)
+                verticalLineTo(9.375f)
+                curveTo(20.25f, 9.9963f, 19.7463f, 10.5f, 19.125f, 10.5f)
+                horizontalLineTo(14.625f)
+                curveTo(14.0037f, 10.5f, 13.5f, 9.9963f, 13.5f, 9.375f)
+                verticalLineTo(4.875f)
+                close()
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(6.75f, 6.75f)
+                horizontalLineTo(7.5f)
+                verticalLineTo(7.5f)
+                horizontalLineTo(6.75f)
+                verticalLineTo(6.75f)
+                close()
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(6.75f, 16.5f)
+                horizontalLineTo(7.5f)
+                verticalLineTo(17.25f)
+                horizontalLineTo(6.75f)
+                verticalLineTo(16.5f)
+                close()
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(16.5f, 6.75f)
+                horizontalLineTo(17.25f)
+                verticalLineTo(7.5f)
+                horizontalLineTo(16.5f)
+                verticalLineTo(6.75f)
+                close()
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(13.5f, 13.5f)
+                horizontalLineTo(14.25f)
+                verticalLineTo(14.25f)
+                horizontalLineTo(13.5f)
+                verticalLineTo(13.5f)
+                close()
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(13.5f, 19.5f)
+                horizontalLineTo(14.25f)
+                verticalLineTo(20.25f)
+                horizontalLineTo(13.5f)
+                verticalLineTo(19.5f)
+                close()
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(19.5f, 13.5f)
+                horizontalLineTo(20.25f)
+                verticalLineTo(14.25f)
+                horizontalLineTo(19.5f)
+                verticalLineTo(13.5f)
+                close()
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(19.5f, 19.5f)
+                horizontalLineTo(20.25f)
+                verticalLineTo(20.25f)
+                horizontalLineTo(19.5f)
+                verticalLineTo(19.5f)
+                close()
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(16.5f, 16.5f)
+                horizontalLineTo(17.25f)
+                verticalLineTo(17.25f)
+                horizontalLineTo(16.5f)
+                verticalLineTo(16.5f)
+                close()
+            }
+        }.build()
+        return _QrCode!!
+    }
+
+private var _QrCode: ImageVector? = null
--- app/src/main/java/hu/klm60o/spiritrally/screens/NewsScreen.kt
+++ app/src/main/java/hu/klm60o/spiritrally/screens/NewsScreen.kt
@@ -4,18 +4,122 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import hu.klm60o.spiritrally.assets.QrCode
 import hu.klm60o.spiritrally.screens.ui.theme.SpiritRallyTheme
 
 @Composable
 fun NewsScreenComposable() {
-
+    Surface {
+        BottomAppBar(
+            actions = {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(10.dp, 0.dp, 10.dp, 0.dp)
+                ) {
+                    IconButton(onClick = { /*TODO*/}) {
+                        Icon(Icons.Filled.Home, contentDescription = "News screen")
+                    }
+                    Text(text = "Hírek",
+                        modifier = Modifier
+                            .padding(0.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 10.sp
+                    )
+                }
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(10.dp, 0.dp, 10.dp, 0.dp)
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Info, contentDescription = "Info screen")
+                    }
+                    Text(text = "Térkép",
+                        modifier = Modifier
+                            .padding(0.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 10.sp
+                    )
+                }
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(10.dp, 0.dp, 10.dp, 0.dp)
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.CheckCircle, contentDescription = "Results screen")
+                    }
+                    Text(text = "Eredmények",
+                        modifier = Modifier
+                            .padding(0.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 10.sp
+                    )
+                }
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(10.dp, 0.dp, 10.dp, 0.dp)
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Person, contentDescription = "Profile screen")
+                    }
+                    Text(text = "Profil",
+                        modifier = Modifier
+                            .padding(0.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 10.sp
+                    )
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    containerColor = BottomAppBarDefaults.containerColor,
+                    elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                ) {
+                    Icon(QrCode, contentDescription = "Read QR code")
+                }
+            }
+        )
+    }
 }
 
 @Composable
@@ -28,8 +132,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview2() {
-    SpiritRallyTheme {
-        Greeting("Android")
+fun LoginPreview() {
+    hu.klm60o.spiritrally.ui.theme.ui.theme.SpiritRallyTheme {
+        NewsScreenComposable()
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreviewDark() {
+    hu.klm60o.spiritrally.ui.theme.SpiritRallyTheme(darkTheme = true) {
+        NewsScreenComposable()
+    }
+}
+
