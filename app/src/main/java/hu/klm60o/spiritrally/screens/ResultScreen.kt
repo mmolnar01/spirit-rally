c599065df29092529fd906c1d3cdab15cb0faf2f
Molnár Márton <127486642+mmolnar01@users.noreply.github.com>
2024-11-08 13:07:26 +0100

    Update ResultScreen.kt

--- app/src/main/java/hu/klm60o/spiritrally/screens/ResultScreen.kt
+++ app/src/main/java/hu/klm60o/spiritrally/screens/ResultScreen.kt
@@ -7,26 +7,40 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import hu.klm60o.spiritrally.assets.ErrorIcon
 import hu.klm60o.spiritrally.data.RacePoint
 import hu.klm60o.spiritrally.data.UserViewModel
 import hu.klm60o.spiritrally.useful.getUserDataFromFirestore
+import hu.klm60o.spiritrally.useful.validateEmail
 
 @Composable
 fun ResultScreenComposable(navController: NavController, viewModel: UserViewModel) {
@@ -46,7 +60,7 @@ fun ResultScreenComposable(navController: NavController, viewModel: UserViewMode
         bottomBar = { MyBottomAppbarComposable(navController) }
     ) {
         innerPadding ->
-        Column(verticalArrangement = Arrangement.Center,
+        Column(verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxHeight()
@@ -83,28 +97,71 @@ fun RacePointComposable(racePoint: RacePoint, size: Int) {
             .padding(5.dp)
     ) {
         if (racePoint.id == 1) {
-            Icon(
-                Icons.Filled.Place, contentDescription = "Start Point Icon"
-            )
-            Text(
-                "Rajt"
-            )
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Icon(
+                    Icons.Filled.Place, contentDescription = "Start Point Icon"
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Text(
+                    "Rajt"
+                )
+            }
+
         } else if (racePoint.id == size) {
-            Icon(
-                Icons.Filled.Place, contentDescription = "End Point Icon"
-            )
-            Text(
-                "Cél"
-            )
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Icon(
+                    Icons.Filled.Place, contentDescription = "End Point Icon"
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Text(
+                    "Cél"
+                )
+            }
+
         } else {
             val racePointNumber = racePoint.id?.minus(1)
-            Icon(
-                Icons.Filled.Place, contentDescription = "Intermediate Point Icon"
-            )
-            Text(
-                "$racePointNumber. ellenörző pont"
-            )
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Icon(
+                    Icons.Filled.Place, contentDescription = "Intermediate Point Icon"
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Text(
+                    "$racePointNumber. ellenörző pont"
+                )
+            }
+
         }
+
+        Column(
+            modifier = Modifier
+                .padding(5.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.End
+        ) {
+            Text("Oje")
+        }
+
 
     }
 }
